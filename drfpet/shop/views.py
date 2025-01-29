5f9aea33ceb93d8365b150a5fb0a6603fcd987a8
MarleyStansh <485262323@mail.ru>
2025-01-29 19:34:37 +0300

    Added pagination

--- drfpet/shop/views.py
+++ drfpet/shop/views.py
@@ -2,6 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .serializers import HardwareSerializer
@@ -9,8 +10,14 @@ from .models import Hardware, Category
 from .permissions import IsAdminOrReadOnly
 
 
+class ShopAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
 class HardwareViewSet(viewsets.ModelViewSet):
     serializer_class = HardwareSerializer
+    pagination_class = ShopAPIListPagination
 
     def get_queryset(self):
         pk = self.kwargs.get('pk')
